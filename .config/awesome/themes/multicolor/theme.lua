ed15cb373eb8f2b7340f1ae95ca0860c6e111782
Rahul <68483218+ghostr72@users.noreply.github.com>
2021-08-17 14:40:29 +0530

    wall update

--- .config/awesome/themes/multicolor/theme.lua
+++ .config/awesome/themes/multicolor/theme.lua
@@ -16,7 +16,7 @@ local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
 theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
-theme.wallpaper                                 = theme.confdir .. "/wall.png"
+theme.wallpaper                                 = theme.confdir .. "/wall.jpg"
 theme.font                                      = "JetBrains Mono 8"
 theme.menu_bg_normal                            = "#000000"
 theme.menu_bg_focus                             = "#000000"
